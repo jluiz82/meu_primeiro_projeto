fb62b6a6b4f0f3881b84a816748384352b145236
jluiz82 <jefersonccomp@gmail.com>
2021-12-28 16:39:38 -0300

    Expressão condicional

--- hello_world.rb
+++ hello_world.rb
@@ -1,3 +1,13 @@
 5.times do
     puts 'Hello World'
 end
+
+# Comparação (expressão condicional)
+
+nome = "Jeferson" # Atribuição
+
+if nome == "Matias"
+    puts 'Autorizado'
+else
+    puts 'Não Autorizado'
+end
